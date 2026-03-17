ee4ff075ab9255dcbdbf2b3484a0298d7bfada47
Erick Almeida <102871441+DEVerickreal@users.noreply.github.com>
2026-03-17 03:44:39 -0300

    Refactor Discord bot setup and command registration
    Refactored bot initialization and command registration.

--- index.js
+++ index.js
@@ -14,4 +14,39 @@ client.on('messageCreate', message => {
   }
 });
 
+const comandos = require('./comandos');
+
+const { Client, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const comandos = require('./comandos');
+
+const CLIENT_ID = 'CLI1483337697526419527';
+const GUILD_ID = 'GUILD_ID1381967839094636624';
+
+const rest = new REST({ version: '9' }).setToken('SEU_TOKEN');
+
+(async () => {
+    try {
+        await rest.put(Routes.applicationGuildCommands(CLI1483337697526419527, GUILD_ID1381967839094636624), {
+            body: [comandos.data.toJSON()],
+        });
+        console.log('Comandos registrados com sucesso!');
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const command = interaction.commandName;
+
+    if (command === 'teste') {
+        await comandos.execute(interaction);
+    }
+});
+
+client.login('MTQ4MzMzNzY5NzUyNjQxOTUyNw.GKfHH4.dYtNm9GXyy5DAMCb6ENf92MaeD0Ufsq6QKbDaU');
+
 client.login(process.env.TOKEN);
